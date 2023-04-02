3bad2c247cc6b04b026f764ea7ffa372e62753fe
Aleksey <easy.mail.to.copy@gmail.com>
2023-04-02 17:15:26 +0300

    Solved microtask with filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,11 @@
-import React, { useState } from "react";
-import { NewComponent } from "./NewComponent";
-import { Button } from "./Button";
-import { UseStateLearn } from "./UseState";
+import { Filter } from "./Filter";
 
 function App() {
-  //   const [students, setStudents] = useState([
-  //     { id: 1, name: "James", age: 8 },
-  //     { id: 2, name: "Robert", age: 18 },
-  //     { id: 3, name: "John", age: 28 },
-  //     { id: 4, name: "Michael", age: 38 },
-  //     { id: 5, name: "William", age: 48 },
-  //     { id: 6, name: "David", age: 58 },
-  //     { id: 7, name: "Richard", age: 68 },
-  //     { id: 8, name: "Joseph", age: 78 },
-  //     { id: 9, name: "Thomas", age: 88 },
-  //     { id: 10, name: "Charles", age: 98 },
-  //     { id: 11, name: "Christopher", age: 100 },
-  //   ]);
 
-  //   const topCars = [
-  //     { manufacturer: "BMW", model: "X5" },
-  //     { manufacturer: "Mersedes", model: "MLS" },
-  //     { manufacturer: "Audi", model: "Q7" },
-  //   ];
- 
-
-  const ButtonFoo1 = (name:string, age:number, adress:string) => {
-    console.log(name, age, adress)
-  }
-
-  const ButtonFoo2 = (name:string) => {
-    console.log(name)
-  }
-
-  const ButtonFoo3 = () => {
-    console.log("I'am stupid button")
-  }
-
-  
 
   return (
     <div className="App">
-      {/* <NewComponent students={students} cars={topCars}/> */}
-      {/* <Button name={"Button 1"} callback={()=> ButtonFoo1("Anrew", 29, "live in London")}/>
-      <Button name={"Button 2"} callback={()=> ButtonFoo2("Sanya")}/>
-      <Button name={"Stupid Button"} callback={ButtonFoo3}/> */}
-      <UseStateLearn/>
+      <Filter/>
     </div>
   );
 }
--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -1,46 +1,34 @@
-type StudentType = {
-  id: number;
-  name: string;
-  age: number;
-};
+import {FitlerType} from "./Filter"
 
-type ArrayComponentType = {
-  students: Array<StudentType>;
-  cars: Array<topCarType>;
-};
+type NewComponentType = {
+    currentMoney: Array<CurrentMoneyType>
+    onClickFilterHandler: (el: FitlerType) => void
+}
 
-type topCarType = {
-  manufacturer: string;
-  model: string;
-};
-
-export const NewComponent = (props: ArrayComponentType) => {
-  return (
-    <div>
-      <ol>
-        {props.students.map((el) => {
-          return (
-            <li key={el.id}>
-              {el.name} age: {el.age}
-            </li>
-          );
-        })}
-      </ol>
-      <table>
-        {props.cars.map((el, index) => {
-          debugger;
-          return ( 
-            <span>
-              <tr>
-                <th key={index + 1}>{el.manufacturer}</th>
-              </tr>
-              <tr>
-                <td key={index + 1}>{el.model}</td>
-              </tr>
-            </span>
-          );
-        })}
-      </table>
-    </div>
-  );
+type CurrentMoneyType = {
+    banknote: string
+    nominal: number
+    number: string
+}
+export const NewComponent = (props: NewComponentType) => {
+    return (
+        <>
+            <ul>
+                {props.currentMoney.map((objFromMoneyArr, index) => {
+                    return (
+                        <li key={index + 1}>
+                            <span>{objFromMoneyArr.banknote}</span>
+                            <span>{objFromMoneyArr.nominal}</span>
+                            <span>{objFromMoneyArr.number}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+            <div style={{marginLeft: '35px'}}>
+                <button onClick={() => props.onClickFilterHandler('All')}>All</button>
+                <button onClick={() => props.onClickFilterHandler('Rubles')}>Rubles</button>
+                <button onClick={() => props.onClickFilterHandler('Dollars')}>Dollars</button>
+            </div>
+        </>
+    );
 };
